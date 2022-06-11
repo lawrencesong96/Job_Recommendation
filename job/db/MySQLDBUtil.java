9a0aa119680d65981e4ec33ee9928dfb56ab8c8d
lawrencesong96 <72828121+lawrencesong96@users.noreply.github.com>
2022-06-11 17:19:10 -0500

    Update MySQLDBUtil.java

--- job/db/MySQLDBUtil.java
+++ job/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package com.laioffer.job.db;
 
 public class MySQLDBUtil {
-    private static final String INSTANCE = "laiproject-instance.c103clgyesbz.us-east-2.rds.amazonaws.com";
-    private static final String PORT_NUM = "3306";
+    private static final String INSTANCE = "laiproject-instance.xxxxxxxx.rds.amazonaws.com";   //link of AWS RDS instance
+    private static final String PORT_NUM = "xxxx";
     public static final String DB_NAME = "laiproject";
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = "songchaohui";
+    private static final String PASSWORD = "xxxxxxxxxx";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
